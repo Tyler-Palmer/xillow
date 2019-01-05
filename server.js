7cf2c1549af82e46ae5eb47dadb4c173f6ed7523
Tyler <tyler.william.palmer@gmail.com>
2019-01-05 00:07:58 -0700

    updated error handler

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 8000
+const expressJwt = require("express-jwt")
 require("dotenv").config()
 
 //Middleware
 app.use(express.json())
 app.use(morgan('dev'))
 
-//Routes
 
+app.use("/api", expressJwt({secret: process.env.SECRET}))
+
+//Routes
+app.use("/auth", require("./routes.auth"))
 
 //Database
 mongoose.connect('mongodb://http:localhost:27017',{ useNewUrlParser:true }, () => {
@@ -18,8 +22,12 @@ mongoose.connect('mongodb://http:localhost:27017',{ useNewUrlParser:true }, () =
 })
 
 //Error Handling
-app.use((res,req,next,err) => {
-    return res.status(500).send(err)
+app.use((err,req, res,next) => {
+    console.error(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
+    return res.send({message: err.message})
 })
 
 // Server listen
