3cbdd3263e1bbf9cc2d6b728e721f4c0b14054e6
Duc Pham <bloomsybusiness@yahoo.com>
2019-01-05 04:13:33 -0700

    finished header

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,75 @@
+import React from "react"
+import Nav from "../Navbar/Nav"
+import homeVideo from "../../assets/home-video.mp4"
+import leggyGirl from "../../assets/leggy-girl.mp4"
+import firePits from "../../assets/fire-pits.mp4"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+class Header extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isActive: 1,
+            slideNumber: 0,
+            texts: ["Find your way home", "Find your next rental", "Your Home on Xillow"],
+            videos: [homeVideo, leggyGirl, firePits]
+        }
+        this.myRef = React.createRef()
+    }
+
+    handleSlide = (activeNumber, slideNumber) => {
+        this.setState({
+            isActive: activeNumber,
+            slideNumber
+        }, () =>{
+            this.refs.video.load();
+        })
+    }
+
+    render() {
+        return (
+            <div className="header-container">
+                <Nav />
+                <div className="header-small-container">
+                    <div className="header-small-container__search">
+                        <div className="header-small-container__form">
+                            <h1>{this.state.texts[this.state.slideNumber]}</h1>
+                            <div className="header-small-container__buttons">
+                                <button
+                                    className="transparent-button"
+                                    onClick={() => this.handleSlide(1, 0)}
+                                    style = {{backgroundColor: this.state.isActive === 1 && "rgba(0,116,228,0.7)"}}>
+                                    Buy
+                                    </button>
+                                <button
+                                    className="transparent-button"
+                                    onClick={() => this.handleSlide(2, 1)}
+                                    style = {{backgroundColor: this.state.isActive === 2 && "rgba(0,116,228,0.7)"}}>
+                                    Rent
+                                    </button>
+                                <button
+                                    className="transparent-button"
+                                    onClick={() => this.handleSlide(3, 2)}
+                                    style = {{backgroundColor: this.state.isActive === 3 && "rgba(0,116,228,0.7)"}}>
+                                    Zestimate
+                                    </button>
+                            </div>
+                            <form>
+                                <input placeholder="Enter an adress, neighborhood, city, or ZIP code" />
+                                <button><FontAwesomeIcon icon="search" /></button>
+                            </form>
+                        </div>
+                    </div>
+                    {this.state.slideNumber === this.state.slideNumber &&
+                    <video className="header-small-container__image" loop autoPlay ref="video">
+                        <source src={this.state.videos[this.state.slideNumber]} type="video/mp4" />
+                        <source src={this.state.videos[this.state.slideNumber]} type="video/ogg" />
+                    </video>
+                    }
+                </div>
+            </div >
+        )
+    }
+}
+
+export default Header;
