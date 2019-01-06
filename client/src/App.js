0d5a44d796af99b83dda903a58bd1e3cf980fb7e
Duc Pham <bloomsybusiness@yahoo.com>
2019-01-05 22:37:24 -0700

    nav bar responsive

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,57 @@ import Header from "./Components/Header/Header"
 import Testimony from "./Components/Testimony/Testimony"
 import Footer from "./Components/Footer/Footer"
 import { Switch, Route } from "react-router-dom"
+import Nav from "./Components/Navbar/Nav"
 
-const App = () => {
-    return (
-        <div>
-            <Switch>
-                <Route path="/" render={props =>
-                    <Fragment>
-                        <Header {...props} />
-                        <Testimony {...props} />
-                    </Fragment>}
-                />
-            </Switch>
-            <Footer />
-        </div>
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            currentWidth: window.innerWidth,
+            alredyChange: false,
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.handleResize)
+    }
+
+    handleResize = () => {
+        if (window.innerWidth <= 768 && !this.state.alredyChange) {
+            this.setState({
+                currentWidth: window.innerWidth,
+                alredyChange: true
+            })
+        }
+        else if (window.innerWidth > 769 && this.state.alredyChange) {
+            this.setState({
+                currentWidth: window.innerWidth,
+                alredyChange: false
+            })
+        }
+    }
+
+    render() {
+        console.log(this.state.currentWidth)
+        return (
+            <div>
+                <Nav currentWidth = {this.state.currentWidth} alreadyChange = {this.state.alredyChange} />
+                <Switch>
+                    <Route path="/" render={props =>
+                        <Fragment>
+                            <Header {...props} />
+                            <Testimony {...props} />
+                        </Fragment>}
+                    />
+                </Switch>
+                <Footer />
+            </div>
+        )
+    }
 }
 
 export default App;
