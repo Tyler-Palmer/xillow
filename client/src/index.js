8346109dcebc83ae9b8bdd7dd66ec5ce305586a0
Duc Pham <bloomsybusiness@yahoo.com>
2019-01-05 17:48:56 -0700

    master bugs

--- client/src/index.js
+++ client/src/index.js
@@ -5,26 +5,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./ComponentStyles/_styles.scss"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-<<<<<<< Sub-Footer
-import { faSearch, faCircle, faQuoteLeft, faHome,  faCopyright } from '@fortawesome/free-solid-svg-icons'
-library.add(fab, faSearch, faCircle, faQuoteLeft, faHome,  faCopyright);
+import { faSearch, faCircle, faQuoteLeft, faHome, faCopyright } from '@fortawesome/free-solid-svg-icons'
+import { BrowserRouter } from "react-router-dom";
+import UserProvider from "./Context/UserProvider"
+
+library.add(fab, faSearch, faCircle, faQuoteLeft, faHome, faCopyright);
+
 
 ReactDOM.render(
-=======
-import { faSearch, faCircle, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
-library.add(fab, faSearch, faCircle, faQuoteLeft);
-import { BrowserRouter } from 'react-router-dom'
-import UserProvider from '.Context/UserProvider'
-
-ReactDOM.render(<BrowserRouter>
-    <UserProvider>
-        <App />
-    </UserProvider>
-</BrowserRouter>, document.getElementById("root"))
->>>>>>> master
-
-        <App />
-, document.getElementById("root"))
+    <BrowserRouter>
+        <UserProvider>
+            <App />
+        </UserProvider>
+    </BrowserRouter>
+    , document.getElementById("root"))
 
 //call this when you want font awesome, not included fa <FontAwesomeIcon icon="search" />
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
