b62a80909c636c462dad97bd3701f09ce7db3d2f
Duc Pham <bloomsybusiness@yahoo.com>
2019-01-08 15:54:11 -0700

    add get routes

--- routes/listingsServer.js
+++ routes/listingsServer.js
@@ -3,6 +3,27 @@ const listingServerRouter = express.Router()
 const ListingSchema = require("../models/listingServer")
 
 
+listingServerRouter.get("/", (req, res, next) =>{
+    ListingSchema.find((err, data) =>{
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(data)
+    })
+})
+
+listingServerRouter.get("/:id", (req, res, next) =>{
+    ListingSchema.findOne({_id: req.params.id}, (err, data) =>{
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(data)
+    })
+})
+
+
 listingServerRouter.post("/", (req, res, next) => {
     const newListing = new ListingSchema(req.body);
     newListing.save((err, data)=>{
