18fe13cd2cea0fb05ea2e754fe70b162baef078f
Duc Pham <bloomsybusiness@yahoo.com>
2019-01-09 02:59:32 -0700

    add icon images

---
+++ client/src/Components/GoogleMap/GoogleIcons.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+class GoogleIcon extends React.Component {
+    render() {
+        return (
+            <div>
+                {
+                    this.props.house ? <FontAwesomeIcon className = "home-icon" icon="home" /> :
+                      
+                            <div className="google-icon__image" style={{ backgroundImage: `url(${this.props.icon})` }}></div>
+                     
+                }
+            </div>
+        )
+
+    }
+}
+
+export default GoogleIcon
+{/* <FontAwesomeIcon icon = "map-marker"  className = "map-mark-icon" >
+</FontAwesomeIcon> */}
