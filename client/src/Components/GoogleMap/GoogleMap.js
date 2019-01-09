1434c940ce8eb961e01342d0292265dfcd5b1fd3
Duc Pham <bloomsybusiness@yahoo.com>
2019-01-08 17:18:48 -0700

    finialized folder

---
+++ client/src/Components/GoogleMap/GoogleMap.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+
+class GoogleMap extends Component{
+    static default = {
+        center: {
+            lat: this.props.lat || 59.9,
+            long: this.prop.long || 30.33,
+        },
+        zoom: 15
+    }
+    render(){
+        return(
+            <div style = {{height: "100vh", width: "50vw"}}>
+                <GoogleMapReact 
+                bootstrapURLKeys = {{key: process.env.REACT_APP_GOOGLEKEY}}
+                defaultCenter = {this.props.center}
+                defaultZoom = {this.props.zoom}
+                >
+                    
+                </GoogleMapReact>
+            </div>
+        )
+    }
+}
+
+export default GoogleMap
