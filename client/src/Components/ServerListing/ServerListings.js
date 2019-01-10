430ab9c4c5aa13933a2208858d15c8de4d073b8a
Duc Pham <bloomsybusiness@yahoo.com>
2019-01-09 23:01:43 -0700

    finished functionality of scroll

--- client/src/Components/ServerListing/ServerListings.js
+++ client/src/Components/ServerListing/ServerListings.js
@@ -1,21 +1,26 @@
-import React from "react"
+import React, { Fragment } from "react"
 import ServerListing from "./ServerListing"
 import { withServerListing } from "../../Context/ServerListingContext"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class ServerListings extends React.Component {
     constructor() {
         super();
         this.state = {
-            currentListing: 1
+            currentListing: 1,
+            isChecking: false,
+
         }
     }
     componentDidMount() {
         this.props.getListingData()
     }
 
-    handleSwitchingListing = () => {
-        if (this.state.currentListing <= 3) {
+    handleSwitchingListing = (id) => {
+        if (this.state.currentListing >= 4) {
+            this.setState({
+                currentListing: 1
+            })
         } else {
             this.setState(prevState => ({
                 currentListing: prevState.currentListing + 1
@@ -23,14 +28,66 @@ class ServerListings extends React.Component {
         }
     }
 
+    handleLeftArrow = () => {
+        if(this.state.currentListing <= 1){
+            this.setState({
+                currentListing: 4
+            })
+        } else{
+            this.setState(prevState => ({
+                currentListing: prevState.currentListing - 1
+            }))
+        }
+    }
+
+    handleMouseOver = () => {
+        this.setState({
+            isChecking: true
+        })
+    }
+
+    handleMouseLeave = () =>{
+        this.setState({
+            isChecking: false
+        })
+    }
+
     render() {
+        console.log(this.state.currentListing)
         return (
-            <div className = "listings__container">
+            <div className="listings__container" onMouseOver = {this.handleMouseOver} onMouseLeave = {this.handleMouseLeave} >
                 <h5>Our Listings in Salt Lake City: </h5>
-                <div className="listings__small-container">
-                    {this.props.listingsData.map(each => <ServerListing {...each} />)}
+                {this.state.isChecking &&
+                <FontAwesomeIcon icon="arrow-alt-circle-left" className ="left-arrow-icons" onClick = {this.handleLeftArrow}/>
+                }
+
+    
+                <div className={`listings__small-container ${this.state.currentListing === 1 ? "is-selected-listing" : "not-selected-listing"}`}>
+                        {this.props.listingsData.slice().splice(0.5).map(each => <ServerListing {...each} />)}
                 </div>
+            
+
+                <div className={`listings__small-container ${this.state.currentListing === 2 ? "is-selected-listing" : "not-selected-listing"}`}>
+                        {this.props.listingsData.slice().splice(5.10).map(each => <ServerListing {...each} />)}
+                </div>
+
+
+    
+                <div className={`listings__small-container ${this.state.currentListing === 3 ? "is-selected-listing" : "not-selected-listing"}`}>
+                        {this.props.listingsData.slice().splice(10.15).map(each => <ServerListing {...each} />)}
+                </div>
+                
+
+                <div className={`listings__small-container ${this.state.currentListing === 4 ? "is-selected-listing" : "not-selected-listing"}`}>
+                        {this.props.listingsData.slice().splice(15.20).map(each => <ServerListing {...each} />)}
+                </div>
+                
+
+                {this.state.isChecking &&
+                <FontAwesomeIcon icon="arrow-alt-circle-right" className ="right-arrow-icons"  onClick = {this.handleSwitchingListing}/>
+                }
             </div>
+
         )
     }
 }
