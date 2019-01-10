7ecc1559227b55190ac4e8328a512a5a7679ca33
Duc Pham <bloomsybusiness@yahoo.com>
2019-01-10 14:06:16 -0700

    finished new section

--- client/src/Components/News/New.js
+++ client/src/Components/News/New.js
@@ -1,9 +1,9 @@
-import React from "react"
+import React, { Fragment } from "react"
 import moment from "moment"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const New = (props) => {
-    
+
     const generateClass = () => {
         if (props.sku < 3) {
             return "first-section"
@@ -16,30 +16,44 @@ const New = (props) => {
     const generateTool = () => {
         const randomNumber = Math.floor(Math.random() * 3)
         console.log(randomNumber);
-            if(randomNumber === 1){
-                return "home"
-            }
-            else if(randomNumber === 2){
-                return "lightbulb"
-            }
-            else {
-                return "wrench"
-            }
+        if (randomNumber === 1) {
+            return "home"
+        }
+        else if (randomNumber === 2) {
+            return "lightbulb"
+        }
+        else {
+            return "wrench"
+        }
     }
 
     return (
-        <div className = {generateClass()} style ={{display: generateClass() === "section" && !props.urlToImage && "none"}}>
+        <div className={generateClass()} style={{ display: generateClass() === "section" && !props.urlToImage && "none" }}>
             <div
                 className="xillow__new-image"
-                style={{ backgroundImage: generateClass() === "section" && `url(${props.urlToImage})`, display: generateClass() === "first-section" && "none"}}>
+                style={{ backgroundImage: generateClass() === "section" && `url(${props.urlToImage})`, display: generateClass() === "first-section" && "none" }}>
             </div>
-            <div>
-                <p className = "xillow__new-tools"><FontAwesomeIcon className = "xillow__new-tool" icon ={generateTool()}/></p>
-            </div>
-            <h4 className = "xillow__new-title">{props.title}</h4>
-            <div className = "xillow__new-source">
-                <p><b style={{color: "#0074e4"}}>{props.author}</b> {moment(props.publishedAt).format("lll")}</p>
-            </div>
+            {generateClass() === "first-section" ?
+                <Fragment>
+                    <div>
+                        <p className="xillow__new-tools"><FontAwesomeIcon className="xillow__new-tool" icon={generateTool()} />{generateClass() === "section" && <p>/ Story <FontAwesomeIcon icon="pencil-alt" /> </p>}</p>
+                    </div>
+                    <h4 className="xillow__new-title">{props.title}</h4>
+                    <div className="xillow__new-source">
+                        <p><b style={{ color: "#0074e4" }}>{props.author}</b> {moment(props.publishedAt).format("lll")}</p>
+                    </div>
+                </Fragment>
+                :
+                <div className = "xillow__new-small-container">
+                    <div>
+                        <p className="xillow__new-tools"><FontAwesomeIcon className="xillow__new-tool" icon={generateTool()} />{generateClass() === "section" && <p>/ Story <FontAwesomeIcon icon="pencil-alt" /> </p>}</p>
+                    </div>
+                    <h4 className="xillow__new-title">{props.title}</h4>
+                    <div className="xillow__new-source">
+                        <p><b style={{ color: "#0074e4" }}>{props.author}</b> {moment(props.publishedAt).format("lll")}</p>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
