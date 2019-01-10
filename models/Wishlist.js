bee9e8384fa891d857648c031a48b6760ce47d00
Tyler <tyler.william.palmer@gmail.com>
2019-01-09 17:56:23 -0700

    starting wishlist

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const WishlistSchema = new Schema({
-    placeholder:{
-        type: String
-    },
     user: {
         type: Schema.Types.ObjectId,
         ref: "User",
+    },
+    wishlistItem: {
+        type: Schema.Types.ObjectId,
+        ref: "ListingCollection"
     }
 })
 
