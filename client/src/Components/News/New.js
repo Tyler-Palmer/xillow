3af9cf0e8fb9091287e2b5972b5199d371638a3d
Duc Pham <bloomsybusiness@yahoo.com>
2019-01-10 11:16:50 -0700

    add new image

--- client/src/Components/News/New.js
+++ client/src/Components/News/New.js
@@ -3,10 +3,26 @@ import moment from "moment"
 
 const New = (props) => {
     console.log(props)
+
+    const generateClass = () => {
+        if (props.sku < 3) {
+            return "first-section"
+        }
+        else {
+            return "section"
+        }
+    }
+
     return (
         <div>
-            <h2>{props.title}</h2>
+            <div
+                className="xillow__new-image"
+                style={{ backgroundImage: generateClass() === "section" && `url(${props.urlToImage})`, display: generateClass() === "first-section" && "none"}}>
+            </div>
+            <div>
 
+            </div>
+            <h2>{props.title}</h2>
             <div>
                 <h5>{props.source.name}</h5>
                 <p>{props.author}</p>
--- client/src/Components/News/News.js
+++ client/src/Components/News/News.js
@@ -11,13 +11,16 @@ class News extends React.Component {
     render() {
         return (
             <div className="xillow-news__container">
-                <div className="xillow-news__background" style={{ backgroundImage: url(`url(https://wp.zillowstatic.com/1/cropped-shutterstock_281623898-78366a-b1fc53-672ba1.jpg)`) }}>
-                    <div>
-                        {this.props.newsData.slice().splice(0, 3).map(each => <New {...each} />)}
+                <div className="xillow-news__background" style={{ backgroundImage: `url(https://wp.zillowstatic.com/1/cropped-shutterstock_281623898-78366a-b1fc53-672ba1.jpg)` }}>
+                    <div className="xillow-new__first-group">
+                        {this.props.newsData.slice().splice(0, 3).map((each, id) => <New {...each} sku = {id}/>)}
                     </div>
                 </div>
                 <div className="xillow-news__new">
-                    {this.props.newsData.slice().splice(3, 10).map(each => <New {...each} />)}
+                    {this.props.newsData.slice().splice(3, 10).map((each, id) => <New {...each} sku = {id + 3} />)}
+                </div>
+                <div className="xillow-news__new">
+                    {this.props.newsData.slice().splice(10, this.props.newsData.length).map((each, id) => <New {...each} sku = {id + 10} />)}
                 </div>
             </div>
         )
