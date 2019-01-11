f1f320aaee4fbf3f20530d69074271b490eb609e
Tyler <tyler.william.palmer@gmail.com>
2019-01-11 07:23:19 -0700

    maps page search bar

---
+++ client/src/Components/Search/SubSearch.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { withGoogleData } from "../../Context/NearbyData"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+class SubSearch extends Component {
+    constructor() {
+        super()
+        this.state = {
+            address: ""
+        }
+    }
+
+    handleUserSearchInput = (e) => {
+        const { name, value } = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleUserSearchSubmit = (e) => {
+        e.preventDefault()
+        this.props.getNearbyLocationData(this.state.address)
+        this.props.history.push("/search")
+    }
+
+    render() {
+        return (
+            <div className="sub-search-container">
+                <div className="sub-search-container-small">
+                    <form onSubmit={this.handleUserSearchSubmit}>
+                        <input
+                            name="address"
+                            value={this.state.address}
+                            onChange={this.handleUserSearchInput}
+                            placeholder="Enter an adress, neighborhood, city, or ZIP code" />
+                        <button><FontAwesomeIcon icon="search" /></button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default withGoogleData(SubSearch)
