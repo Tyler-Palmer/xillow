5d0247fd4c427328be053c1d9f0fbf9a0a230635
Tyler-Palmer <tyler.william.palmer@gmail.com>
2019-02-05 21:54:33 -0700

    deploy configurations

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 8000
 const expressJwt = require("express-jwt")
+const path = require("path")
 
 
 //Middleware
 app.use(express.json({limit: '200mb'}))
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 
 app.use("/api", expressJwt({secret: process.env.SECRET}))
@@ -20,7 +22,7 @@ app.use("/listing", require("./routes/listingsServer"))
 app.use("/mortgages", require("./routes/mortgages"))
 app.use("/savedhouse", require("./routes/SavedHouse"))
 //Database
-mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true }, () => {
+mongoose.connect(process.env.MONGOD.URI ||'mongodb://localhost:27017/myapp', {useNewUrlParser: true }, () => {
     console.log("The database is connected, Guv'nor!")
 })
 
@@ -31,6 +33,10 @@ app.use((err,req, res,next) => {
         res.status(err.status)
     }
     return res.send({message: err.message})
+})
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
 
 // Server listen
